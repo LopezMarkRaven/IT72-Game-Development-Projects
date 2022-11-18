7620a21b05cbfd380750c38bbee381c8e37f50cf
LopezMarkRaven <116812992+LopezMarkRaven@users.noreply.github.com>
2022-11-18 18:20:49 +0800

    3dmaze2

---
+++ My project 3Dmaze/Assets/player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody rb;
+    float movementSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity =new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+
+    }
+}
